57691e2f9706f1522460768059856130a2a2aa1e
JoanWaithira <joanwaithira.jw@gmail.com>
2024-04-03 05:43:48 +0000

    Task two

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'main.css',
+        }),
+        new ImageMinimizerPlugin({
+            minimizerOptions: {
+                plugins: [
+                    ['imagemin-mozjpeg', { quality: 80 }],
+                    ['imagemin-optipng', { optimizationLevel: 5 }],
+                ],
+            },
+        }),
+    ],
+};
+
